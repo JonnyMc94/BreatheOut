804d8693b27044aac3fda53586dd96ed95925c19
JonnyMc94 <jonathanmc.jm94@gmail.com>
2021-02-22 11:36:11 +0000

    Yoga and meditation components on homepage and start on choices page

---
+++ src/pages/ContentCard.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './pages.css'
+
+const ContentCard = (props) => {
+  return (
+  <div className="card text-center"  >
+      <div className="overflow">
+        <img src={props.imgsrc} alt={ props.imgalt }  style={{height: "400px", width: "100%"}}/>
+      </div>
+      <div className="card-img-overlay text-light" style={{ paddingTop: "60px" } }>
+        <h1 className="card-title">{ props.cardTitle }</h1>
+        <p className="card-text text-light">
+         { props.cardText }
+        </p>
+        <a href="#" className="btn btn-success">Go Anywhere</a>
+      </div>
+  </div>
+  
+  
+
+  )};
+
+export default ContentCard;
---
+++ src/pages/MeditationChoice.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const MeditationChoice = () => {
+  return (
+  <div className="container-fluid">
+    <div class="form-group">
+      <select class="custom-select">
+        <option selected="">Please select your answer</option>
+        <option value="1">One</option>
+        <option value="2">Two</option>
+        <option value="3">Three</option>
+      </select>
+    </div>
+  </div>
+  
+  
+
+  )};
+
+export default MeditationChoice;
